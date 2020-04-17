fbb0ed54ea2acf018dc5a3801178a4cc5d791160
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-04-17 03:06:02 -0300

    Renamed TransformTest to TransformContextTest +utils

--- tst/utils.h
+++ tst/utils.h
@@ -15,5 +15,19 @@ double distance(const T *a, const T *b, size_t length)
     return sqrt(_distance);
 }
 
+/* Euclidean distance between two arrays */
+template <typename T>
+double distance_percent(const T *a, const T *b, size_t length) 
+{
+    double _distance = 0;
+    double _size_a = 0;
+    for (size_t i = 0; i < length; i++) {
+        _distance += pow(a[i] - b[i], 2);
+        _size_a += pow(a[i], 2);
+    }
+    return sqrt(_distance) / sqrt(_size_a);
+}
+
+
 
 #endif // __TEST_UTILS_H__
