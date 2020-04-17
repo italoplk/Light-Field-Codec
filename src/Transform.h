2eceb70b55c65b6b2a76822dbb973c0846d76429
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-04-16 23:04:45 -0300

    class Transform now uses the new API It will be kept for backwards compatibility with older code. New code should be using the new API TransformContext.h

--- src/Transform.h
+++ src/Transform.h
@@ -5,42 +5,21 @@
 #include <zconf.h>
 #include "Typedef.h"
 #include "Point4D.h"
+#include "TransformContext.h"
 #include <cmath>
 
 class Transform {
 private:
-    float *coeff_dct1D[4]{};
-    Point4D dim_block;
-
-    float *input_4D_t;
-
-    static float *generate_dct_coeff(int N);
-
-    static void dct_1D(const float *in, float *out, float *coeff, const uint offset, const uint size);
-
-    static void idct_1D(const float *in, float *out, float *coeff, const uint offset, const uint size);
-
+    Point4D stride_lightfield;
+    Point4D size_lightfield;
+    TransformContext<float> *ctx;
 
 public:
     explicit Transform(const Point4D &dimBlock);
-
-    void foward(const float *input, float *output);
-
-    void inverse(const float *input, float *output);
-
-    void dct_4d(const float *input, float *output, const Point4D &size, const Point4D &origSize);
-
-    void idct_4d(const float *input, float *output, const Point4D &size, const Point4D &origSize);
-
     ~Transform();
 
-    void delete_coeff_dct();
-
-    void alocate_coeff_dct(const Point4D &dimBlock);
-
-    void foward(const short *input, float *output);
-
-    void inverse(const float *input, short *output);
+    void dct_4d(const float *input, float *output, const Point4D &size, const Point4D &origSize);
+    void idct_4d(const float *input, float *output, const Point4D &size, const Point4D &origSize);
 };
 
 
