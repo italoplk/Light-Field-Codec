3d413faabb66bd7f7b557a5a949b892ac79ad0b8
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-04-17 06:23:55 -0300

    Refactored the API to allow reuse of code. DiscreteCosineTransformContext uses the Mixin OrthogonalTransformMixin

--- src/DiscreteCosineTransformContext.h
+++ src/DiscreteCosineTransformContext.h
@@ -5,17 +5,19 @@
 #include <stdexcept>
 #include <cmath>
 #include "TransformContext.h"
-
+#include "OrthogonalTransformMixin.h"
 
 template <typename T>
-class DiscreteCosineTransformContext : public TransformContext<T> {
-public:
+class _DiscreteCosineTransformContext : public TransformContext<T>
+{
+private:
+    const size_t _size;
+    const size_t _stride;
+protected:
     static std::map<int, const T*> coeff_cache;
 
-
-    /* Generate the DCT coefficients.
-     * Never checks whether they were created previously or not. */
-    static const T *generate_dct_coeff(const size_t size)
+    /* Implementation of the helper method to calculate the DCT coefficients */
+    static const T *generate_coefficients(const size_t size)
     {
         auto N = size;
         auto *output = new T[N * N];
@@ -27,130 +29,34 @@ public:
                 *p_output++ = s * (double) cos((double) M_PI * (2 * n + 1) * k / (2 * N));
         }
         return output;
-
     }
-
-    /* Search in the cache for the coefficients by a given dimension.
-     * If one is not found, generate a new one and return.
-     */
-    static const T *get_coeff(const size_t size)
-    {
-        // BUG: Race conditions?
-        try {
-            // Just returns whatever is stored at coeff_cache[size].
-            return coeff_cache.at(size);
-        } catch(const std::out_of_range& e) {
-            // Expected exception for cases where coeff_cache[size] is invalid.
-            const T* coeff = generate_dct_coeff(size);
-            coeff_cache[size] = coeff;
-            return coeff;
-        }
-    }
-
-    // 1D DCT II implementation.
-    // TODO: Improve this algorithm to use vector notaton.
-    void dct(const T *in, T *out, const size_t _size)
-    {
-        const T *pin;
-        T *pout = out;
-        const T *coeff = get_coeff(_size);
-
-        // out[k] = in[k] * coeff[k][n] for n=0..size
-        for (int k = 0; k < _size; ++k) {
-            double sum = 0;
-            pin = in;
-            
-            for (int n = 0; n < _size; ++n) {
-                sum += *pin * *coeff;
-                pin += _stride;
-                coeff++;
-            }
-
-            *pout = sum;
-            pout += _stride;
-        }
-    }
-
-    // 1D DCT III implementation.
-    // TODO: Improve this algorithm to use vector notaton.
-    void idct(const T *in, T *out, const size_t _size)
-    {
-        const T *pin;
-        T *pout = out;
-        const T *pcoeff;
-        const T *coeff = get_coeff(_size);
-
-        // out[k] = in[k] * coeff[n][k] for n=0..size
-        for (int k = 0; k < _size; ++k) {
-            float sum = 0;
-            float partial_sums[_size];
-            pin = in;
-            pcoeff = &coeff[k];
-            for (int n = 0; n < _size; ++n) {
-                sum += *pin * *pcoeff;
-                pin += _stride;
-                pcoeff += _size;
-                partial_sums[n] = sum;
-            }
-            *pout = sum;
-            pout += _stride;
-        }
-    }
-
-    const size_t _size;
-    const size_t _stride;
-
-public:
-    DiscreteCosineTransformContext(size_t size_, size_t stride_ = 1)
+    _DiscreteCosineTransformContext(size_t size_, size_t stride_ = 1)
     : _size(size_), _stride(stride_)
     {
         // Update parent pointers to correct values.
         TransformContext<T>::size = &_size;
         TransformContext<T>::stride = &_stride;
     }
-    DiscreteCosineTransformContext(size_t * size_, size_t * stride_)
+    _DiscreteCosineTransformContext(size_t * size_, size_t * stride_)
     {
         // Update parent pointers to correct values.
-        TransformContext<T>::size = size_;
-        TransformContext<T>::stride = stride_;
-        _size = *TransformContext<T>::size;
-        _stride = *TransformContext<T>::stride;
+        _size = *size_;
+        _stride = *stride_;
+        TransformContext<T>::size = &_size;
+        TransformContext<T>::stride = &_stride;
     } 
-
-    /* Forward application of the DST on a single dimension */
-    void forward(const T *input, T *output)
-    {
-        dct(input, output, _size);
-    }
-    void forward(const T *input, T *output, const size_t *size)
-    {
-        dct(input, output, *size);
-    }
-
-    /* Inverse application of the DST on a single dimension */
-    void inverse(const T *input, T *output)
-    {   
-        idct(input, output, _size);
-    }
-    void inverse(const T *input, T *output, const size_t *size)
-    {
-        idct(input, output, *size);
-    }
-
-    // Silently drops every entry in coeff_cache. 
-    static void flush_coeff()
-    {
-        for (const auto& kv : coeff_cache) 
-            delete[] kv.second;
-        coeff_cache.clear();
-    }
-    
-
 };
 
 
+/** Declaration of static member */
 template <typename T>
-std::map<int, const T*> DiscreteCosineTransformContext<T>::coeff_cache;
+std::map<int, const T*> _DiscreteCosineTransformContext<T>::coeff_cache;
+
+/** Exports the name DiscreteCosineTransformContext with the mixin 
+ * OrthogonalTransformContext */
+template <typename T>
+using DiscreteCosineTransformContext = OrthogonalTransformMixin<_DiscreteCosineTransformContext<T>>;
+
 
 
 #endif // __DISCRETECOSINETRANSFORMCONTEXT_H__
---
+++ src/OrthogonalTransformMixin.h
@@ -0,0 +1,109 @@
+#if !defined(__ORTHOGONALTRANSFORMMixin_H__)
+#define __ORTHOGONALTRANSFORMMixin_H__
+
+
+
+/** Orthogonal Transforms follow these two steps:
+ * 1) Calculate its coefficients matrix
+ * 2) Multiplication between input array and coefficients matrix
+ * 
+ * To provide the same interface to execute the same steps, this
+ * mixin should inherit from the any class that defines only the
+ * method to calculate the coefficients.
+ * The necessary methods implementation of .forward() and .inverse()
+ * needed for TransformContext<T> are done here. 
+ * 
+ * To use this class, you need to:
+ * 1) Inherit from TransformContext
+ * 2) Implement the default constructors for (const size_t, const size_t) 
+ * and (const size_t*, const size_t*)
+ * 3) Declare a static property std::map<int, const T*> coeff_cache
+ * 4) Use as following:  OrthogonalTransformContext<YourType<T>> *ctx;
+ */
+template <typename BASE, typename T = typename BASE::value_type>
+class OrthogonalTransformMixin : public BASE
+{
+private:
+    /** Method to populate BASE::coeff_cache with the coefficients to be used
+     * throuhout the application. Ii assumes BASE::generate_coefficients from
+     * the base class. */
+    static const T *get_coefficients(const size_t size)
+    {
+        try {
+            // Just returns whatever is stored at coeff_cache[size].
+            return BASE::coeff_cache.at(size);
+        } catch(const std::out_of_range& e) {
+            // Expected exception for cases where coeff_cache[size] is invalid.
+            const T* coeff = BASE::generate_coefficients(size);
+            BASE::coeff_cache[size] = coeff;
+            return coeff;
+        }
+    }
+
+    /** Method that multiplies the input array with the coefficients matrix.
+     * It allows for the transpose run of the coefficients matrix to calculate
+     * the inverse transform as well. */
+    void ortho_transform(const T* coefficients,
+                         const T *array,
+                         T *result,
+                         const bool transpose_matrix)
+    {
+        auto *size = BASE::size;
+        auto *stride = BASE::stride;
+        size_t index_coeff;
+        T sum;
+        for (int k = 0; k < *size; k++) {
+            sum = 0;
+            for (int n = 0; n < *size; n++) {
+                index_coeff = transpose_matrix ? n * *size + k : k * *size + n;
+                sum += array[n * *stride] * coefficients[index_coeff];
+            }
+            result[k * *stride] = sum;
+        }
+    }
+
+   
+public:
+    const size_t *size;
+    const size_t *stride;
+    
+    OrthogonalTransformMixin(size_t size_, size_t stride_ = 1)
+    : BASE{ size_, stride_ }
+    { }
+
+    OrthogonalTransformMixin(size_t *size_, size_t *stride_)
+    : BASE{ size_, stride_ }
+    { }
+
+
+
+    void forward(const T *input, T *output) 
+    {
+        forward(input, output, BASE::size);
+    }
+    void forward(const T *input, T *output, const size_t *size)
+    {
+        auto *coeff = get_coefficients(*size);
+        ortho_transform(coeff, input, output, /* dont transpose */ false);
+    }
+
+    void inverse(const T *input, T *output)
+    {
+       inverse(input, output, BASE::size);
+    }
+    void inverse(const T *input, T *output, const size_t *size)
+    {
+        auto *coeff = get_coefficients(*size);
+        ortho_transform(coeff, input, output, /* transpose */ true);
+    }
+    // Silently drops every entry in coeff_cache. 
+    static void flush_coeff()
+    {
+        for (const auto& kv : BASE::coeff_cache) 
+            delete[] kv.second;
+        BASE::coeff_cache.clear();
+    }
+};
+
+
+#endif // __ORTHOGONALTRANSFORMMixin_H__
