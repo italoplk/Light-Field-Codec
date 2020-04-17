b8799a9365992d99967b7af9690d2b1892d513e5
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-04-16 23:54:23 -0300

    Test for the recovered signal through DCT is within 1%

--- tst/TransformTest.cpp
+++ tst/TransformTest.cpp
@@ -18,7 +18,8 @@
 #include "../src/DiscreteCosineTransformContext4D.h"
 
 
-#define ERROR_EPSILON 1e-6
+#define ERROR_EPSILON               1e-6
+#define ERROR_REC_EPSILON           1e-2
 
 auto gtestlog = spdlog::basic_logger_mt("gtestlog", "logs/gtestlog.txt");
 
@@ -70,7 +71,8 @@ double distance(const T *a, const T *b, size_t length)
  * It caused small changes that, if accumulated, grew beyond 1e-5 in arrays for 
  * 100 elements.  
  */
-TEST(ContextTest, new_dct_api_is_consistent_to_old_api) {
+TEST(BackwardsCompatibilityWithOldAPI, new_dct_1d_is_consistent)
+{
     #define FULL_LENGTH 512 
     TransformContext<float> *ctx;
 
@@ -119,7 +121,8 @@ TEST(ContextTest, new_dct_api_is_consistent_to_old_api) {
     #undef FULL_LENGTH 
 }
 
-TEST(ContextTest, new_dct_api_4d_is_consistent_to_old_api) {
+TEST(BackwardsCompatibilityWithOldAPI, new_dct_4d_is_consistent)
+{
     #define SIZE_X  15
     #define SIZE_Y  16
     #define SIZE_U  17
@@ -181,7 +184,8 @@ TEST(ContextTest, new_dct_api_4d_is_consistent_to_old_api) {
     #undef SIZE_V
 }
 
-TEST(ContextTest, new_dct_API_works_for_blocks_of_different_sizes) {
+TEST(BackwardsCompatibilityWithOldAPI, new_api_accepts_blocks_of_smaller_sizes)
+{
     #define SIZE_X  15
     #define SIZE_Y  16
     #define SIZE_U  17
@@ -251,4 +255,85 @@ TEST(ContextTest, new_dct_API_works_for_blocks_of_different_sizes) {
     #undef SIZE_V
     #undef FULL_LENGTH
     #undef DELTA_SIZE
+}
+
+TEST(DCTContextTest, dct_error_is_within_1_percent)
+{
+    #define FULL_LENGTH 512 
+    TransformContext<float> *ctx;
+
+    float signal[FULL_LENGTH];
+    float dct_signal[FULL_LENGTH];
+    float recovered_signal[FULL_LENGTH];
+
+    // Seed the random number generator
+    std::srand(0);
+
+    // Populate signal with random numbers.
+    for (int i = 0; i < FULL_LENGTH; i++) {
+        float value = 256 * (std::rand() / (RAND_MAX * 1.0));
+        signal[i] = value;
+        dct_signal[i] = 0;
+        recovered_signal[i] = 0;
+    }
+    
+    ctx = new DiscreteCosineTransformContext<float>(FULL_LENGTH);
+
+    // Forward DCT
+    ctx->forward(signal, dct_signal);
+    ctx->inverse(dct_signal, recovered_signal);
+
+    EXPECT_NEAR(distance<float>(signal, recovered_signal, FULL_LENGTH), 
+        0, ERROR_REC_EPSILON) << "Distance between original and recovered is "
+                                 "bigger than 1%.";
+    
+    delete ctx;
+    DiscreteCosineTransformContext<float>::flush_coeff();
+    #undef FULL_LENGTH 
+}
+
+TEST(DCTContextTest, dct_4d_error_is_within_1_percent)
+{
+    #define SIZE_X  15
+    #define SIZE_Y  16
+    #define SIZE_U  17
+    #define SIZE_V  18
+    #define FULL_LENGTH (SIZE_X * SIZE_Y * SIZE_U * SIZE_V)
+
+    Point4D size(SIZE_X, SIZE_Y, SIZE_U, SIZE_V);
+    Point4D stride(1,
+                   SIZE_X,
+                   SIZE_X * SIZE_Y, 
+                   SIZE_X * SIZE_Y * SIZE_U);
+
+    float signal[FULL_LENGTH];
+    float dct_signal[FULL_LENGTH];
+    float recovered_signal[FULL_LENGTH];
+    
+    TransformContext<float> *ctx;
+
+    // Seed the random number generator
+    std::srand(0);
+
+    // Populate signal with random numbers.
+    for (int i = 0; i < FULL_LENGTH; i++) {
+        float value = 256 * (std::rand() / (RAND_MAX * 1.0));
+        signal[i] = value;
+        dct_signal[i] = 0;
+        recovered_signal[i] = 0;
+    }
+    
+    ctx = new DiscreteCosineTransformContext4D<float>(size, stride);
+
+    // Forward DCT
+    ctx->forward(signal, dct_signal);
+    ctx->inverse(dct_signal, recovered_signal);
+
+    EXPECT_NEAR(distance<float>(signal, recovered_signal, FULL_LENGTH), 
+        0, ERROR_REC_EPSILON) << "Distance between original and recovered is "
+                                 "bigger than 1%.";
+    
+    delete ctx;
+    DiscreteCosineTransformContext<float>::flush_coeff();
+    #undef FULL_LENGTH 
 }
