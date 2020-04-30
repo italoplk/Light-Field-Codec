dfd8130d8e7f589218357fdd8afe0acffe7f63d0
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-04-30 20:00:00 -0300

    new functionality and exception log. Block.h logs to a file everyexception. Block.view can be chained with other views. See test.

--- src/Block.h
+++ src/Block.h
@@ -2,185 +2,215 @@
 #define __BLOCK_H__
 
 #include <cstdarg>
-#include <stdexcept>
-#include <sstream>
-#include <iostream>
 #include <initializer_list>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
+
+auto logger = spdlog::basic_logger_mt("block_logger", "logs/block.txt");
 
 template <typename T>
-class Block
-{
-public:
-    struct Range
-    {
-        int begin;
-        int end;
+std::string join(const std::string &delim, const T *begin, const T *end) {
+  std::stringstream s;
+  for (auto it = begin; it != end; it++) {
+    if (it != begin) {
+      s << delim;
+    }
+    s << *it;
+  }
+  return s.str();
+}
 
-        Range(int begin = -1, int end = -1)
-            : begin(begin), end(end)
-        {
-        }
-    };
+template <typename T> std::string join(const std::string &delim, const T &v) {
+  std::stringstream s;
+  for (auto it = std::begin(v); it != std::end(v); it++) {
+    if (it != std::begin(v)) {
+      s << delim;
+    }
+    s << *it;
+  }
+  return s.str();
+}
+
+template <typename T> class Block {
+public:
+  struct Range {
+    int begin;
+    int end;
+
+    Range(int begin = -1, int end = -1) : begin(begin), end(end) {}
+    friend std::ostream &operator<<(std::ostream &os, const Range &r) {
+      os << r.str();
+      return os;
+    }
+    std::string str() const {
+      std::stringstream s;
+
+      s << "Range(begin=" << (begin == -1 ?: begin)
+        << (begin == -1 ? "None" : "") << ", end=" << (end == -1 ?: end)
+        << (end == -1 ? "None" : "") << ")";
+      return s.str();
+    }
+  };
 
 private:
-    T *array;
-    size_t size_dim;
-    size_t *size;
-    size_t *stride = nullptr;
-    Range *ranges = nullptr;
+  T *array;
+  size_t size_dim;
+  size_t *size;
+  size_t *stride = nullptr;
+  Range *ranges = nullptr;
 
-    inline size_t _index(const size_t *index)
-    {
-        // Dimension checks
-        for (int i = 0; i < size_dim; i++)
-            if (index[size_dim - 1 - i] >= size[i])
-            {
-                std::stringstream message("Tried to access position ");
-                message << index;
-                message << " beyound size ";
-                message << size;
-                message << " at dimension ";
-                message << i;
-                throw std::out_of_range(message.str());
-            }
-        size_t idx = 0;
-        for (int i = 0; i < size_dim; i++)
-        {
-            auto rev_i = size_dim - 1 - i;
-            idx += (index[rev_i] + ranges[rev_i].begin) * stride[i];
-        }
-
-        return idx;
+  inline size_t _index(const size_t *index) {
+    // Dimension checks
+    for (int i = 0; i < size_dim; i++)
+      if (index[size_dim - 1 - i] >= size[i]) {
+        logger->error("_index: invalid position. index={{{}}}, shape={{{}}}",
+                      join<size_t>(", ", index, index + size_dim),
+                      join<size_t>(", ", size, size + size_dim));
+        throw std::out_of_range("Tried to access invalid position.");
+      }
+    size_t idx = 0;
+    for (int i = 0; i < size_dim; i++) {
+      auto rev_i = size_dim - 1 - i;
+      idx += (index[rev_i] + ranges[rev_i].begin) * stride[i];
     }
+    return idx;
+  }
 
-    inline void recalculate_stride()
-    {
-        if (!stride)
-            delete[] stride;
-        stride = new size_t[size_dim + 1];
-        stride[0] = 1;
-        for (int d = 0; d < size_dim; d++)
-            stride[d + 1] = size[d] * stride[d];
-    }
+  inline void recalculate_stride() {
+    if (stride)
+      delete[] stride;
+    stride = new size_t[size_dim + 1];
+    stride[0] = 1;
+    for (int d = 0; d < size_dim; d++)
+      stride[d + 1] = size[d] * stride[d];
+  }
 
-    inline void recalculate_ranges()
-    {
-        if (ranges)
-            delete[] ranges;
-        ranges = new Range[size_dim];
-        for (int i = 0; i < size_dim; i++)
-        {
-            ranges[i].begin = 0;
-            ranges[i].end = size[i];
-        }
+  inline void recalculate_ranges() {
+    if (ranges)
+      delete[] ranges;
+    ranges = new Range[size_dim];
+    for (int i = 0; i < size_dim; i++) {
+      ranges[i].begin = 0;
+      ranges[i].end = size[i];
     }
+  }
 
-    Block(const Block &b, const std::initializer_list<Range> &ranges)
-    {
-        array = b.array;
-        size_dim = b.size_dim;
-        size = new size_t[size_dim];
-        for (int i = 0; i < size_dim; i++)
-            size[i] = b.size[i];
-        recalculate_stride();
-        this->ranges = new Range[size_dim];
-        auto *r = this->ranges;
-        for (auto it = ranges.begin(); it != ranges.end(); it++)
-            *r++ = *it;
+  Block(const Block &b, const std::vector<Range> &ranges) {
+    array = b.array;
+    size_dim = b.size_dim;
+    size = new size_t[size_dim];
+    for (int i = 0; i < size_dim; i++)
+      size[i] = b.size[i];
+    recalculate_stride();
+    this->ranges = new Range[size_dim];
+    for (int i = 0; i < size_dim; i++) {
+      this->ranges[i].begin = ranges[i].begin + b.ranges[i].begin;
+      this->ranges[i].end = ranges[i].end + b.ranges[i].begin;
     }
+    logger->debug("Block: got ranges: {}. Updated ranges: {}",
+                  join<std::vector<Range>>(", ", ranges),
+                  join<Range>(", ", this->ranges, this->ranges + size_dim));
+  }
 
 public:
-    Block(T *array_, const size_t size)
-        : Block(array_, &size, 1)
-    {
+  Block(T *array_, const size_t size) : Block(array_, &size, 1) {}
+
+  Block(T *array_, const size_t *size_, const size_t size_dim_)
+      : array(array_) {
+    logger->set_level(spdlog::level::debug);
+    if (!array) {
+      logger->error("Block: Tried to wrap an invalid memory address: {X}",
+                    (void *)array);
+      throw std::invalid_argument("Invalid pointer.");
+    }
+    size_dim = size_dim_;
+    size = new size_t[size_dim];
+
+    for (int i = 0; i < size_dim; i++)
+      size[i] = size_[i];
+
+    stride = nullptr;
+    recalculate_stride();
+    recalculate_ranges();
+  }
+
+  T &operator()(const size_t index0, ...) {
+    // Array to index underlying "tensor"
+    size_t index[size_dim];
+    index[0] = index0;
+
+    // Read variadic argument list into index[]
+    va_list ap;
+    va_start(ap, index0);
+    for (int i = 1; i < size_dim; i++)
+      index[i] = va_arg(ap, size_t);
+    va_end(ap);
+
+    return array[_index(index)];
+  }
+
+  void reshape(std::initializer_list<size_t> dim) {
+    size_t total_elements = 1;
+
+    for (auto it = dim.begin(); it != dim.end(); it++)
+      total_elements *= *it;
+
+    if (total_elements != stride[size_dim]) {
+      logger->error("reshape: invalid shape. Current {{{}}}, got {{{}}}",
+                    join<size_t>(", ", size, size + size_dim),
+                    join<size_t>(", ", dim.begin(), dim.end()));
+      throw std::length_error("Invalid shape");
     }
 
-    Block(T *array_, const size_t *size_, const size_t size_dim_)
-        : array(array_)
-    {
-        size_dim = size_dim_;
-        size = new size_t[size_dim];
+    size_t *new_size = new size_t[dim.size()];
+    size_dim = dim.size();
+    size_t *p = new_size;
+    for (auto it = dim.begin(); it != dim.end(); it++)
+      *p++ = *it;
+    delete[] size;
+    size = new_size;
+    recalculate_stride();
+    recalculate_ranges();
+  }
 
-        for (int i = 0; i < size_dim; i++)
-        {
-            size[i] = size_[i];
-        }
-        stride = nullptr;
-        recalculate_stride();
-        recalculate_ranges();
+  auto view(std::initializer_list<Range> ranges) {
+    const char *message;
+    if (ranges.size() != size_dim) {
+      logger->error("view: nedded {} ranges. Got {}", size_dim, ranges.size());
+      throw std::invalid_argument(
+          "Number of Range instances does not match dimension");
     }
-
-    T &operator()(const size_t index0, ...)
-    {
-        // Array to index underlying "tensor"
-        size_t index[size_dim];
-        index[0] = index0;
-
-        // Read variadic argument list into index[]
-        va_list ap;
-        va_start(ap, index0);
-        for (int i = 1; i < size_dim; i++)
-            index[i] = va_arg(ap, size_t);
-        va_end(ap);
-
-        return array[_index(index)];
+    int i = 0;
+    std::vector<Range> adjusted_ranges;
+    for (auto &it : ranges) {
+      auto begin = 0;
+      auto end = this->ranges[i].end - this->ranges[i].begin;
+      if ((it.begin != -1 && it.begin < begin) ||
+          (it.end != -1 && it.end > end)) {
+        logger->error("view: invalid range. Current {}, got {}",
+                      this->ranges[i].str(), it.str());
+        message = "Invalid Range";
+        throw std::out_of_range(message);
+      }
+      if (it.begin != -1 && it.end != -1 && it.end < it.begin) {
+        logger->error(
+            "view: decreasing indexing not allowed. Current {}, got {}",
+            this->ranges[i].str(), it.str());
+        throw std::invalid_argument("Decreasing indexing not allowed");
+      }
+      i++;
+      // auto r = Range();
+      // r.begin = it.begin == -1 ? -1 : it.begin + this->ranges[i].begin - 1;
+      // r.end = it.end == -1 ? -1 : it.end + this->ranges[i].begin - 1;
+      // adjusted_ranges.push_back(r);
     }
-
-    void reshape(std::initializer_list<size_t> dim)
-    {
-        size_t total_elements = 1;
-
-        for (auto it = dim.begin(); it != dim.end(); it++)
-            total_elements *= *it;
-
-        if (total_elements != stride[size_dim])
-        {
-            std::stringstream message("Invalid shape ");
-            std::copy(dim.begin(),
-                      dim.end(),
-                      std::ostream_iterator<int>(message, " "));
-            throw std::length_error(message.str());
-        }
-
-        size_t *new_size = new size_t[dim.size()];
-        size_dim = dim.size();
-        size_t *p = new_size;
-        for (auto it = dim.begin(); it != dim.end(); it++)
-            *p++ = *it;
-        delete[] size;
-        size = new_size;
-        recalculate_stride();
-        recalculate_ranges();
-    }
-
-    auto view(std::initializer_list<Range> ranges)
-    {
-        bool ranges_are_good = true;
-        const char *message;
-        if (ranges.size() != size_dim)
-        {
-            ranges_are_good = false;
-            message = "Number of Range instances does not match dimension";
-            throw std::invalid_argument(message);
-        }
-        int i = 0;
-        for (const auto &it : ranges)
-        {
-            if ((it.begin != -1 && it.begin < this->ranges[i].begin)
-                || (it.end != -1 && it.end > this->ranges[i].end))
-            {
-                message = "Invalid Range";
-                throw std::out_of_range(message);
-            }
-            if (it.begin != -1 && it.end != -1 && it.end < it.begin)
-            {
-                message = ".end can't be smaller than .begin";
-                throw std::invalid_argument(message);
-            }
-            i++;
-        }
-        return Block(*this, ranges);
-    }
+    return Block(*this, ranges);
+  }
 };
 
 #endif // __BLOCK_H__
--- tst/BlockTest.cpp
+++ tst/BlockTest.cpp
@@ -1,108 +1,137 @@
+#include "Block.h"
 #include <gtest/gtest.h>
 #include <stdexcept>
-#include "Block.h"
 
-TEST(BlockTests, block_uses_same_memory_space_as_original_array)
-{
-    int array[100];
-    Block<int> b(array, 100);
+TEST(BlockTests, block_uses_same_memory_space_as_original_array) {
+  int array[100];
+  Block<int> b(array, 100);
 
-    for (int i = 0; i < 100; i++)
-    {
-        array[i] = i;
-    }
+  for (int i = 0; i < 100; i++) {
+    array[i] = i;
+  }
 
-    bool same_values = true;
-    for (int i = 0; i < 100; i++)
-    {
-        if (b(i) != array[i])
-            same_values = false;
-    }
-    ASSERT_TRUE(same_values);
+  bool same_values = true;
+  for (int i = 0; i < 100; i++) {
+    if (b(i) != array[i])
+      same_values = false;
+  }
+  ASSERT_TRUE(same_values);
 }
 
-TEST(BlockTests, throw_exception_if_tries_to_access_invalid_location)
-{
-    int variable;
-    Block<int> b(&variable, 1);
+TEST(BlockTests, throw_exception_if_tries_to_access_invalid_location) {
+  int variable;
+  Block<int> b(&variable, 1);
 
-    ASSERT_THROW((b(2) = 4), std::out_of_range);
+  ASSERT_THROW((b(2) = 4), std::out_of_range);
 }
 
-TEST(BlockTests, reshape_block)
-{
-    int array[100];
-    Block<int> b(array, 100);
+TEST(BlockTests, reshape_block) {
+  int array[100];
+  Block<int> b(array, 100);
 
-    // Initialize array
-    for (int i = 0; i < 100; i++)
-    {
-        array[i] = i;
-    }
+  // Initialize array
+  for (int i = 0; i < 100; i++) {
+    array[i] = i;
+  }
 
-    b.reshape({10, 10});
+  b.reshape({10, 10});
 
-    bool same_values = true;
-    for (int i = 0; i < 10; i++)
-        for (int j = 0; j < 10; j++)
-            if (b(i, j) != array[i * 10 + j])
-                same_values = false;
+  bool same_values = true;
+  for (int i = 0; i < 10; i++)
+    for (int j = 0; j < 10; j++)
+      if (b(i, j) != array[i * 10 + j])
+        same_values = false;
 
-    ASSERT_TRUE(same_values);
+  ASSERT_TRUE(same_values);
 }
 
-TEST(BlockTests, reshape_invalid_size_throws_exception)
-{
-    int var;
-    Block<int> b(&var, 15);
+TEST(BlockTests, reshape_invalid_size_throws_exception) {
+  int var;
+  Block<int> b(&var, 15);
 
-    EXPECT_NO_THROW(b.reshape({3, 5}));
-    EXPECT_NO_THROW(b.reshape({5, 3}));
-    ASSERT_THROW(b.reshape({1, 10}), std::length_error);
+  EXPECT_NO_THROW(b.reshape({3, 5}));
+  EXPECT_NO_THROW(b.reshape({5, 3}));
+  ASSERT_THROW(b.reshape({1, 10}), std::length_error);
 }
 
-TEST(BlockTests, access_block_elements_through_view)
-{
-    using Range = Block<int>::Range;
-    int array[1000];
-    Block<int> b(array, 1000);
-    b.reshape({10, 10, 10});
+TEST(BlockTests, access_block_elements_through_view) {
+  using Range = Block<int>::Range;
+  int array[1000];
+  Block<int> b(array, 1000);
+  b.reshape({10, 10, 10});
 
-    // Initialize array
-    for (int i = 0; i < 1000; i++)
-    {
-        array[i] = i;
-    }
+  // Initialize array
+  for (int i = 0; i < 1000; i++) {
+    array[i] = i;
+  }
 
-    auto v1 = b.view({Range(0, 5), Range(0, 5), Range(0, 5)}); // First Octant
-    for (int i = 0; i < 5; i++)
-        for (int j = 0; j < 5; j++)
-            for (int k = 0; k < 5; k++)
-                ASSERT_EQ(v1(i, j, k), array[i * 100 + j * 10 + k]);
+  auto v1 = b.view({Range(0, 5), Range(0, 5), Range(0, 5)}); // First Octant
+  for (int i = 0; i < 5; i++)
+    for (int j = 0; j < 5; j++)
+      for (int k = 0; k < 5; k++)
+        ASSERT_EQ(v1(i, j, k), array[i * 100 + j * 10 + k]);
 
-    auto v2 = b.view({Range(6, 10), Range(0, 5), Range(0, 5)}); // Second Octant
-    for (int i = 0; i < 5; i++)
-        for (int j = 0; j < 5; j++)
-            for (int k = 0; k < 5; k++)
-                ASSERT_EQ(v2(i, j, k), array[(i + 6) * 100 + j * 10 + k]);
+  auto v2 = b.view({Range(6, 10), Range(0, 5), Range(0, 5)}); // Second Octant
+  for (int i = 0; i < 5; i++)
+    for (int j = 0; j < 5; j++)
+      for (int k = 0; k < 5; k++)
+        ASSERT_EQ(v2(i, j, k), array[(i + 6) * 100 + j * 10 + k]);
 
-    auto v3 = b.view({Range(2, 8), Range(1, 10), Range(7, 9)}); // view 6 x 9 x 2
-    for (int i = 0; i < 6; i++)
-        for (int j = 0; j < 9; j++)
-            for (int k = 0; k < 2; k++)
-                ASSERT_EQ(v3(i, j, k), array[(i + 2) * 100 + (j + 1) * 10 + (k + 7)]);
+  auto v3 = b.view({Range(2, 8), Range(1, 10), Range(7, 9)}); // view 6 x 9 x 2
+  for (int i = 0; i < 6; i++)
+    for (int j = 0; j < 9; j++)
+      for (int k = 0; k < 2; k++)
+        ASSERT_EQ(v3(i, j, k), array[(i + 2) * 100 + (j + 1) * 10 + (k + 7)]);
 }
 
-TEST(BlockTests, view_with_invalid_ranges_throws_exception)
-{
-    using Range = Block<int>::Range;
-    int array[1000];
-    Block<int> b(array, 1000);
-    b.reshape({10, 10, 10});
+TEST(BlockTests, view_with_invalid_ranges_throws_exception) {
+  using Range = Block<int>::Range;
+  int array[1000];
+  Block<int> b(array, 1000);
+  b.reshape({10, 10, 10});
 
-    ASSERT_THROW(b.view({Range(5, 11), Range(2, 3), Range(3, 4)}), std::out_of_range);
-    ASSERT_THROW(b.view({Range(2, 3), Range(5, 11), Range(3, 4)}), std::out_of_range);
-    ASSERT_THROW(b.view({Range(2, 3), Range(3, 4), Range(5, 11)}), std::out_of_range);
-    ASSERT_THROW(b.view({Range(2, 3), Range(3, 4)}), std::invalid_argument);
-    ASSERT_THROW(b.view({Range(4, 0), Range(2, 3), Range(3, 4)}), std::invalid_argument);
+  ASSERT_THROW(b.view({Range(5, 11), Range(2, 3), Range(3, 4)}),
+               std::out_of_range);
+  ASSERT_THROW(b.view({Range(2, 3), Range(5, 11), Range(3, 4)}),
+               std::out_of_range);
+  ASSERT_THROW(b.view({Range(2, 3), Range(3, 4), Range(5, 11)}),
+               std::out_of_range);
+  ASSERT_THROW(b.view({Range(2, 3), Range(3, 4)}), std::invalid_argument);
+  ASSERT_THROW(b.view({Range(4, 0), Range(2, 3), Range(3, 4)}),
+               std::invalid_argument);
+}
+
+TEST(BlockTests, create_view_of_view) {
+  using Range = Block<int>::Range;
+  int array[8 * 8 * 8];
+  size_t x0, x1, dx, y0, y1, dy, z0, z1, dz;
+  Block<int> b(array, 8 * 8 * 8);
+
+  b.reshape({8, 8, 8});
+  x0 = 3;
+  x1 = 6;
+  dx = x1 - x0;
+  y0 = 2;
+  y1 = 7;
+  dy = y1 - y0;
+  z0 = 1;
+  z1 = 5;
+  dz = z1 - z0;
+
+  // Initialize array
+  for (int i = 0; i < 8 * 8 * 8; i++)
+    array[i] = i;
+
+  // Slicing b three consecutive times
+  auto v1 = b.view({Range(x0, x1), Range(0, 8), Range(0, 8)});
+  auto v2 = v1.view({Range(0, dx), Range(y0, y1), Range(0, 8)});
+  auto v3 = v2.view({Range(0, dx), Range(0, dy), Range(z0, z1)});
+
+  // Slicing b at once.
+  auto v = b.view({Range(x0, x1), Range(y0, y1), Range(z0, z1)});
+
+  for (size_t x = 0; x < dx; x++)
+    for (size_t y = 0; y < dy; y++)
+      for (size_t z = 0; z < dz; z++)
+        ASSERT_EQ(v3(x, y, z), v(x, y, z));
 }
