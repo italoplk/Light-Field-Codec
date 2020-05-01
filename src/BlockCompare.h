5716ffd086e2828695d7a9e6a77e9b9ec25c50c1
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-05-01 20:03:49 -0300

    Added safeguard to blockcompare.h

--- src/BlockCompare.h
+++ src/BlockCompare.h
@@ -1,27 +1,29 @@
+#if !defined(__BLOCKCOMPARATOR_H_)
+#define __BLOCKCOMPARATOR_H_
 
-template <typename T>
-class BlockComparator {
+template <typename T> class BlockComparator {
 
 public:
-    /**
-     * Method to compare two blocks. 
-     * It relies on comparing features and statistical data
-     * of each parameter. At the end, it returns a notion of 
-     * which block has more energy than the other.
-     * 
-     * @param lhs: ponter to the left block.
-     * @param rhs: pointer to the right block.
-     * @return
-     *     -1 if lhs is smaller than rhs
-     *      0 if lhs equal rhs
-     *      1 if lhs is bigger than rhs
-     * 
-     */
-    int compare(const T *lhs, const T *rhs);
+  /**
+   * Method to compare two blocks.
+   * It relies on comparing features and statistical data
+   * of each parameter. At the end, it returns a notion of
+   * which block has more energy than the other.
+   *
+   * @param lhs: ponter to the left block.
+   * @param rhs: pointer to the right block.
+   * @return
+   *     -1 if lhs is smaller than rhs
+   *      0 if lhs equal rhs
+   *      1 if lhs is bigger than rhs
+   *
+   */
+  int compare(const T *lhs, const T *rhs);
 };
 
 template <typename T>
-int BlockComparator<T>::compare(const T *lhs, const T *rhs)
-{
-    // Código aqui.
+int BlockComparator<T>::compare(const T *lhs, const T *rhs) {
+  // Código aqui.
 }
+
+#endif // __BLOCKCOMPARATOR_H_
