9060d26706f0118b6ae9f3c3c85cf53a736ca468
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-05-04 13:50:06 -0300

    Added TUI interface to simulation.py

---
+++ simulation/core/screen.py
@@ -0,0 +1,164 @@
+import curses
+import time
+import sys
+import re
+from simulation import Simulation
+
+start_time = int(time.time())
+
+class SimulationScreen:
+
+    def __init__(self, stdscr, total_jobs):
+        self.H, self.W = stdscr.getmaxyx()
+        self.stdscr = stdscr
+        self.total_jobs = total_jobs
+        self.title_bar = curses.newwin(1, self.W, 0, 0)
+        self.status_bar = curses.newwin(1, self.W, self.H - 1, 0)
+        self.info_pad = curses.newpad(self.total_jobs, 22)
+        self.job_pad = curses.newpad(self.total_jobs, 1024)
+
+        self.info_offset = [0, 0]
+        self.job_offset = [0, 0]
+        self.timers = {}
+        self.jobs_finished = 0
+        self.jobs_instances = []
+        self.shrink_paths = False
+        self.graceful_exit = False
+        self._end_loop = False
+        self.status_message = ''
+
+    def recreate_windows(self):
+        self.H, self.W = self.stdscr.getmaxyx()
+        self.title_bar = curses.newwin(1, self.W, 0, 0)
+        self.status_bar = curses.newwin(1, self.W, self.H - 1, 0)
+        self.info_pad = curses.newpad(self.total_jobs, 22)
+        self.job_pad = curses.newpad(self.total_jobs, 1024)
+        
+        for i, (_, job) in enumerate(Simulation.status.items()):
+            self.info_pad.addstr(i, 0, '[                   ]')
+            self.info_pad.addstr(
+                i, 1, 'AWAITING'.center(19), curses.color_pair(2))
+            parsed_job = job['instance'].parse()
+            if self.shrink_paths:
+                parsed_job = ' '.join(f'{path[:6]}~{path[-8:]}'
+                                      if re.match("([><]?[/\.]+[a-zA-Z\./]*[\s]?)", path) else path
+                                      for path in parsed_job.split())
+            self.job_pad.addstr(i, 0, parsed_job.ljust(1023))
+
+    def elapsed_time(self, timer):
+        start = self.timers.setdefault(timer, time.time())
+        return time.strftime('%H:%M:%S', time.gmtime(time.time() - start))
+
+    def sec2str(self, sec):
+        return time.strftime('%H:%M:%S', time.gmtime(sec))
+
+    def loop(self, parallel_instances):
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        self.stdscr.keypad(True)
+        self.stdscr.nodelay(True)
+
+        Simulation.dispatch_all(parallel_instances)
+        self.recreate_windows()
+        self.refresh_all()
+        
+        while not self._end_loop:
+            c = self.stdscr.getch()
+            if time.time() - self.last_refresh > 0.25:
+                self.refresh_all()
+            if c == ord('q'):
+                self.set_status('Cleaning queue')
+                while not Simulation.instances.empty():
+                    Simulation.instances.get_nowait()
+                self.set_status('Waiting processes to finish')
+            elif c == ord('s'):
+                self.shrink_paths = not self.shrink_paths
+                self.update_jobpad()
+            elif c == curses.KEY_LEFT:
+                self.job_offset[1] = max(self.job_offset[1] - 1, 0)
+                self.update_jobpad()
+            elif c == curses.KEY_RIGHT:
+                self.job_offset[1] = min(self.job_offset[1] + 1, 1024)
+                self.update_jobpad()
+            elif c == curses.KEY_DOWN:
+                height = self.total_jobs - self.H - 2
+                self.job_offset[0] = min(self.job_offset[0] + 1, height)
+                self.info_offset[0] = min(self.info_offset[0] + 1, height)
+                self.update_jobpad()
+                self.update_infopad()
+            elif c == curses.KEY_UP:
+                self.job_offset[0] = max(self.job_offset[0] - 1, 0)
+                self.info_offset[0] = max(self.info_offset[0] - 1, 0)
+                self.update_jobpad()
+                self.update_infopad()
+            elif c == curses.KEY_RESIZE:
+                self.recreate_windows()
+        self.refresh_all()
+        self.set_status('Joining processes and exiting.')
+        Simulation.join()
+        curses.curs_set(1)
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+    def update_titlebar(self):
+        _, width = self.title_bar.getmaxyx()
+        title = ' '.join(sys.argv)
+
+        timestamp = self.elapsed_time('titlebar')
+        line = f' {title:<{width-12}}{timestamp:>9} '
+        self.title_bar.addstr(0, 0, line, curses.A_REVERSE)
+        self.title_bar.refresh()
+
+    def update_statusbar(self):
+        _, width = self.status_bar.getmaxyx()
+        jobs = f' Jobs: {self.jobs_finished}/{self.total_jobs}'
+        line = f'{jobs:<{15}}{self.status_message:>{width - 17}} '
+        self.status_bar.addstr(0, 0, line, curses.A_REVERSE)
+        self.status_bar.refresh()
+
+    def set_status(self, msg):
+        self.status_message = msg
+        self.status_bar.refresh()
+
+    def update_jobpad(self):
+        status = list(Simulation.status.items())
+        for i, (_, job) in enumerate(status):
+            parsed_job = job['instance'].parse()
+            if self.shrink_paths:
+                parsed_job = ' '.join(f'{path[:6]}~{path[-8:]}'
+                                      if re.match("([><]?[/\.]+[a-zA-Z\./]*[\s]?)", path) else path
+                                      for path in parsed_job.split())
+            self.job_pad.addstr(i, 0, parsed_job.ljust(1023))
+        self.job_pad.refresh(*[*self.job_offset, 2, 23, self.H - 2, self.W-2])
+
+    def update_infopad(self):
+        status = list(Simulation.status.items())
+        finished = 0
+        running = 0
+        for i, (_, job) in enumerate(status):
+            if job['running'] and not job['done']:
+                line = f' {self.elapsed_time(f"timer{i}"):<8}'
+                self.info_pad.addstr(i, 1, line.ljust(19))
+                self.info_pad.addstr(i, 11, 'RUNNING', curses.color_pair(1))
+                running += 1
+
+            elif not job['running'] and job['done']:
+                finished += 1
+                seconds = job['time_end'] - job['time_start']
+                line = f' {self.sec2str(seconds):<8}'
+                self.info_pad.addstr(i, 1, line.ljust(19))
+                self.info_pad.addstr(i, 11, ' DONE!', curses.color_pair(3))
+        self.jobs_finished = finished
+        if running == 0:
+            self._end_loop = True
+        self.info_pad.refresh(*[*self.info_offset, 2, 1, self.H - 2, 22])
+
+    def refresh_all(self):
+        self.update_titlebar()
+        self.update_statusbar()
+        self.update_jobpad()
+        self.update_infopad()
+        self.last_refresh = time.time()
--- simulation/core/simulation.py
+++ simulation/core/simulation.py
@@ -1,10 +1,17 @@
 import os
 import subprocess
-from multiprocessing import Process, Queue
+import time
+import random
+import logging
+from itertools import count
+from multiprocessing import Process, Queue, Manager
 
 
 class Simulation:
+    _id_seed = count()
     instances = Queue()
+    instances_status = {}
+    status = None
 
     def __init__(self, executable, args, stdin=None, stdout=None, stderr=None, fake=None):
         self.executable = executable
@@ -13,7 +20,19 @@ class Simulation:
         self.stdout = stdout
         self.stderr = stderr
         self.fake = fake
+        self.id = next(Simulation._id_seed)
+
         Simulation.instances.put(self)
+        Simulation.instances_status.update({
+            self.id: {
+                'running': False,
+                'done': False,
+                'time_start': None,
+                'time_end': None,
+                'worker_pid': None,
+                'instance': self,
+            }
+        })
 
     def parse(self):
         line = f'{self.executable}'
@@ -29,18 +48,23 @@ class Simulation:
 
     @classmethod
     def dispatch_all(cls, parallel_instances=1):
-        process_lst = []
+        cls.process_lst = []
+        cls.manager = Manager()
+        Simulation.status = cls.manager.dict()
+        Simulation.status.update(Simulation.instances_status)
+
         for cpu in range(parallel_instances):
-            p = Process(target=cls._run, args=(cpu, ))
-            process_lst.append(p)
+            p = Process(target=cls._run, args=(Simulation.status, cpu, ))
+            cls.process_lst.append(p)
             p.start()
 
-        for p in process_lst:
+    @classmethod
+    def join(cls):
+        for p in cls.process_lst:
             p.join()
 
-
     @classmethod
-    def _run(cls, cpu_affinity=None):
+    def _run(cls, status, cpu_affinity=None):
         if cpu_affinity:
             os.sched_setaffinity(0, [cpu_affinity])
 
@@ -52,31 +76,59 @@ class Simulation:
 
             args = map(str, sum(instance.args, ()))
             process_info = [instance.executable, *args]
-            print(instance.parse())
 
             if not instance.fake:
                 open_files = {}
                 for stream in ['stdin', 'stdout', 'stderr']:
                     file_stream = getattr(instance, stream)
                     if file_stream:
-                        os.makedirs(os.path.dirname(file_stream), exist_ok=True)
+                        os.makedirs(os.path.dirname(
+                            file_stream), exist_ok=True)
                         open_files.setdefault(stream, open(file_stream, 'w'))
-                
+                val = status[instance.id]
+                val.update({
+                    'running': True,
+                    'time_start': time.time()
+                })
+                status[instance.id] = val
                 subprocess.call(process_info, **open_files)
+                val = status[instance.id]
+                val.update({
+                    'running': False,
+                    'done': True,
+                    'time_end': time.time(),
+                })
+                status[instance.id] = val
 
                 for f in open_files.values():
                     f.close()
+            else:
+                val = status[instance.id]
+                val.update({
+                    'running': True,
+                    'time_start': time.time()
+                })
+                status[instance.id] = val
+                subprocess.call(
+                    ['python3 -c "import time; import random; time.sleep(random.randrange(2, 8))"'], shell=True)
+                val = status[instance.id]
+                val.update({
+                    'running': False,
+                    'done': True,
+                    'time_end': time.time(),
+                })
+                status[instance.id] = val
 
 
 def build_simulations(settings, options):
     simulations = []
     for args in settings.args:
         parsed_values, args = settings.parse_variables(args)
-        
+
         if not options.fake:
             # Create -output path if it does not exist
             os.makedirs(args['-output'], exist_ok=True)
-        
+
         sim = Simulation(settings.EXECUTABLE,
                          list(args.items()),
                          stdin=parsed_values['STDIN'],
