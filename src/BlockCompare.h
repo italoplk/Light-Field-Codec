110725a7d188436d7368a8025fc86a7998babcc2
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-05-03 22:25:31 -0300

    BlockCompare improved with test suit

--- src/BlockCompare.h
+++ src/BlockCompare.h
@@ -15,13 +15,14 @@ public:
    *
    * @param lhs: ponter to the left block.
    * @param rhs: pointer to the right block.
+   * @param size: length of both arrays
    * @return
    *     -1 if lhs is smaller than rhs
    *      0 if lhs equal rhs
    *      1 if lhs is bigger than rhs
    *
    */
-  static int compare(const T *lhs, const T *rhs);
+  static int compare(const T *lhs, const T *rhs, const size_t size);
 
   /* Operator overloads */
   inline bool operator==(const T *other) {
@@ -45,9 +46,9 @@ public:
 };
 
 template <typename T>
-int BlockComparator<T>::compare(const T *lhs, const T *rhs) {
+int BlockComparator<T>::compare(const T *lhs, const T *rhs, const size_t size) {
   // Código aqui.
-  // O tamanho dos arrays é dado pela variavel interna this->size.
+  // O tamanho dos arrays é dado pela variavel size.
   // Podes usar ela para fazer os loops, caso necessário.
   return 0;
 }
