fc90774fd0a130ff143692e544fb83770bd19f72
Cristian Maruan Bosin <cmbosin@inf.ufpel.edu.br>
2020-05-12 21:00:14 -0300

    FIX: simulatior tui more responsive

--- simulation/core/screen.py
+++ simulation/core/screen.py
@@ -6,7 +6,12 @@ from simulation import Simulation
 
 start_time = int(time.time())
 
+
 class SimulationScreen:
+    COLOR_RUNNING = None
+    COLOR_WAITING = None
+    COLOR_DONE = None
+    COLOR_ERROR = None
 
     def __init__(self, stdscr, total_jobs):
         self.H, self.W = stdscr.getmaxyx()
@@ -33,11 +38,11 @@ class SimulationScreen:
         self.status_bar = curses.newwin(1, self.W, self.H - 1, 0)
         self.info_pad = curses.newpad(self.total_jobs, 22)
         self.job_pad = curses.newpad(self.total_jobs, 1024)
-        
+
         for i, (_, job) in enumerate(Simulation.status.items()):
             self.info_pad.addstr(i, 0, '[                   ]')
             self.info_pad.addstr(
-                i, 1, 'AWAITING'.center(19), curses.color_pair(2))
+                i, 1, 'AWAITING'.center(19), self.COLOR_WAITING)
             parsed_job = job['instance'].parse()
             if self.shrink_paths:
                 parsed_job = ' '.join(f'{path[:6]}~{path[-8:]}'
@@ -53,21 +58,22 @@ class SimulationScreen:
         return time.strftime('%H:%M:%S', time.gmtime(sec))
 
     def loop(self, parallel_instances):
-        curses.noecho()
-        curses.cbreak()
-        curses.curs_set(0)
-        self.stdscr.keypad(True)
-        self.stdscr.nodelay(True)
 
         Simulation.dispatch_all(parallel_instances)
         self.recreate_windows()
         self.refresh_all()
-        
+
+        next_c = None
         while not self._end_loop:
-            c = self.stdscr.getch()
             if time.time() - self.last_refresh > 0.25:
                 self.refresh_all()
+            c = next_c
+            next_c = self.stdscr.getch()
+            if next_c != -1:
+                continue
+            
             if c == ord('q'):
+                self.graceful_exit = True
                 self.set_status('Cleaning queue')
                 while not Simulation.instances.empty():
                     Simulation.instances.get_nowait()
@@ -94,14 +100,10 @@ class SimulationScreen:
                 self.update_infopad()
             elif c == curses.KEY_RESIZE:
                 self.recreate_windows()
+
         self.refresh_all()
         self.set_status('Joining processes and exiting.')
         Simulation.join()
-        curses.curs_set(1)
-        curses.nocbreak()
-        self.stdscr.keypad(False)
-        curses.echo()
-        curses.endwin()
 
     def update_titlebar(self):
         _, width = self.title_bar.getmaxyx()
@@ -142,7 +144,7 @@ class SimulationScreen:
             if job['running'] and not job['done']:
                 line = f' {self.elapsed_time(f"timer{i}"):<8}'
                 self.info_pad.addstr(i, 1, line.ljust(19))
-                self.info_pad.addstr(i, 11, 'RUNNING', curses.color_pair(1))
+                self.info_pad.addstr(i, 11, 'RUNNING', self.COLOR_RUNNING)
                 running += 1
 
             elif not job['running'] and job['done']:
@@ -150,7 +152,11 @@ class SimulationScreen:
                 seconds = job['time_end'] - job['time_start']
                 line = f' {self.sec2str(seconds):<8}'
                 self.info_pad.addstr(i, 1, line.ljust(19))
-                self.info_pad.addstr(i, 11, ' DONE!', curses.color_pair(3))
+                self.info_pad.addstr(i, 11, ' DONE!', self.COLOR_DONE)
+            
+            elif self.graceful_exit:
+                self.info_pad.addstr(i, 1, 'CANCELED'.center(19), self.COLOR_ERROR)
+
         self.jobs_finished = finished
         if running == 0:
             self._end_loop = True
@@ -162,3 +168,27 @@ class SimulationScreen:
         self.update_jobpad()
         self.update_infopad()
         self.last_refresh = time.time()
+
+    def cleanup(self):
+        curses.curs_set(1)
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        
+        curses.endwin()
+
+    def init(self):
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        self.stdscr.keypad(True)
+        self.stdscr.nodelay(True)
+
+        self.COLOR_RUNNING = curses.color_pair(1)
+        self.COLOR_WAITING = curses.color_pair(2)
+        self.COLOR_DONE = curses.color_pair(3)
+        self.COLOR_ERROR = curses.color_pair(4)
